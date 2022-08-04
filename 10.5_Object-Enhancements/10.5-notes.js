4bb162b8cbd2276754ecf7204a7eb4ba7f21d978
Russ <russellkohler8@gmail.com>
2022-08-04 14:43:23 -0500

    10.5 Object Enhacements Exercise Completed

---
+++ 10.5_Object-Enhancements/10.5-notes.js
@@ -0,0 +1,69 @@
+// function makePerson(first, last, age) {
+//     return {
+//         first   : first,
+//         last    : last,
+//         age     : age,
+//         isAlive : true
+//     };
+// }
+
+
+function makePerson(first, last, age) {
+    return {
+        first,
+        last,
+        age,
+        isAlive : true
+    };
+}
+
+// const mathStuff = {
+//     x : 200,
+//     add : function(a, b) {
+//         return a + b;
+//     },
+//     square : function(a) {
+//         return a * a;
+//     }
+// };
+
+const mathStuff = {
+    x : 200,
+    add(a, b) {
+        return a + b;
+    },
+    square(a) {
+        return a * b;
+    },
+    multpily : (a,b) => {
+        return a * b;
+    }
+};
+
+// 'periwinkle' '9c88ff'
+// 'bright ube' 'D6A2E8'
+
+// const color = {
+//     'periwinkle' : '9c88ff',
+//     'bright ube' : 'D6A2E8'
+// };
+
+// function makeColor(name, hex) {
+//     const color = {};
+//     color[name] = hax;
+//     color[hex] = name;
+//     return color;
+// }
+function makeColor(name, hex) {
+    return {
+        [name] : hex,
+        [hex]  : name
+    };
+}
+
+const mystery = {
+    [6 + 7] : 'thirteen'
+};
+
+const obj = {};
+obj[6 + 7] = 'thirteen';
